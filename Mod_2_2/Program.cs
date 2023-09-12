98ae4b20e2be256a0b3ea1ea6ffc29f1c687934b
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-09-12 20:57:56 +0600

    Add project files.

---
+++ Mod_2_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mod_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину квадрата:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int P = a + a + a + a;
+            Console.WriteLine("Периметр квадрата равен: " + P);
+        }
+    }
+}
